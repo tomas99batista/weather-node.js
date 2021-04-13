3b40a567325d1386b7d13f9499b9ece21544194b
tsbatista <tsbatista@ubiwhere.com>
2021-04-13 17:21:31 +0100

    updates heroku

--- public/js/app.js
+++ public/js/app.js
@@ -17,7 +17,7 @@ weatherForm.addEventListener("submit", (e) => {
   messageTwo.textContent = "";
 
   console.log(location);
-  fetch(`http://localhost:3000/weather?address=${location}`).then((res) => {
+  fetch(`/weather?address=${location}`).then((res) => {
     res.json().then((data) => {
       if (data.error) {
         messageOne.textContent = data.error;
